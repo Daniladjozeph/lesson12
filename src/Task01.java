12cac329534027df39eb310666c245d93d23292e
Daniladjozeph <02ibezug@gmail.com>
2022-09-08 09:33:15 +0300

    update Task01

--- src/Task01.java
+++ src/Task01.java
@@ -1,7 +1,13 @@
 public class Task01 {
     public static void main(String[]args){
         System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+
+
     }
 }
 
- 
