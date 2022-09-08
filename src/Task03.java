892bd631e7fc216eb22ca4e1cafef0029da02c6b
Daniladjozeph <02ibezug@gmail.com>
2022-09-08 10:25:16 +0300

    uzing static math rondom update3

---
+++ src/Task03.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Random random = new Random();
+                //random flooting
+//        for (int i = 0; i <10; i++) {
+//            System.out.print(random.nextDouble()+"  ");
+//        }
+     int bound = 100;
+        for (int i = 0; i <10; i++) {
+            System.out.print(random.nextDouble()+"  ");
+        }
+    }
+}
